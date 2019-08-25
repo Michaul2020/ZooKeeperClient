372b0ed28b9ee9522aee940e6579cf37c662a740
Michael Zhang <mzhang@Michaels-MacBook-Pro-2.local>
2019-08-24 22:56:16 -0700

    Changed entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Text, View } from "react-native";
-
 import AppNavigator from "./AppNavigator";
 import { Provider } from "react-redux";
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStore } from "redux";
+import { registerRootComponent } from "expo";
 import rootReducer from "./redux/reducers/rootReducer";
 
 const store = createStore(rootReducer);
 const AppContainer = createAppContainer(AppNavigator);
 
-export default function App() {
+const App = () => {
   return (
     <Provider store={store}>
       <AppContainer></AppContainer>
     </Provider>
   );
-}
+};
+
+export default registerRootComponent(App);
